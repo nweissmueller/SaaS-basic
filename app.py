a7b29c34d39b1aeaadf6784174998fbabc3444a3
Nikolas Weissmueller <nikolas.weissmueller@gmail.com>
2023-08-13 21:27:16 -0700

    first full commit

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+from st_paywall import add_auth
+
+st.set_page_config(layout="wide")
+st.title("Basic Saas :rocket:")
+
+# pay and subscription wall
+add_auth(required=True)
+
+# view subsequent code requires authentication + subscription
+st.write(f"Subscription Satus: {st.session_state.user_subscribed}")
+st.write(f"Your email {st.session_state.email} is subscribed!")
+st.write("Yay! :partying_face: You're good to go.")
