d67ca97203aa7b1947fcf47953e5a5dd9cd2f492
Nikolas Weissmueller <nikolas.weissmueller@gmail.com>
2023-08-30 17:47:22 -0700

    recent changes

--- app.py
+++ app.py
@@ -8,6 +8,6 @@ st.title("Basic Saas :rocket:")
 add_auth(required=True)
 
 # view subsequent code requires authentication + subscription
-st.write(f"Subscription Satus: {st.session_state.user_subscribed}")
+st.write(f"Subscription Status: {st.session_state.user_subscribed}")
 st.write(f"Your email {st.session_state.email} is subscribed!")
 st.write("Yay! :partying_face: You're good to go.")
